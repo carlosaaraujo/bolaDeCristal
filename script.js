82ad67f4555fc6725b8384dd546c7e05ca6805dd
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-06-22 21:24:39 -0300

    Add comentários ao JS para maior entendimento

--- script.js
+++ script.js
@@ -25,20 +25,23 @@ const respostas = [
 ];
 
 const fazerPergunta = () => {
+	// Checar se o input está vazio.
 	if (inputPergunta.value == "") {
 		alert("Digite a sua pergunta");
 		return;
 	}
-
+	// Adiciono o atributo disabled a tag button do HTML (evita varias respostas para uma mesma pergunta).
 	buttonPerguntar.setAttribute("disabled", true);
+	// Guarda na variavel um elemento HTML que contem div com a pergunta feita.
 	const pergunta = "<div>" + inputPergunta.value + "</div>";
 
+	// Pega o comprimento do array e usa o floor e random para gerar o indice do array a cada clique no button.
 	const totalRespostas = respostas.length;
 	const numeroAleatorio = Math.floor(Math.random() * totalRespostas);
-
+	// Adiciona ao HTML a pergunta feita e a resposta recebida.
 	elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio];
 	elementoResposta.style.opacity = 1;
-
+	// Após 3 segundos retira a opacidade colocada nos textos e remove o atributo disabled do HTML.
 	setTimeout(() => {
 		elementoResposta.style.opacity = 0;
 		buttonPerguntar.removeAttribute("disabled");
